7a45d254affd546441cb31c6e18dfc2e90477c80
fredbeaud <fredbeaud.fb@gmail.com>
2024-01-13 13:09:56 +0100

    treizieme commit

---
+++ src/components/DetailsProduits.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DetailsProduits = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default DetailsProduits;
---
+++ src/core/CardProduit.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CardProduit = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default CardProduit;
